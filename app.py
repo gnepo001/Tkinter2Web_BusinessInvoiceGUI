90749391e5ece542d019f5b6b6e8609b6ab00c81
Gilbert Nepomuceno <gilbertnepomuceno@Gilberts-MacBook-Air.local>
2022-01-15 18:50:06 -0800

    Update

--- app.py
+++ app.py
@@ -5,28 +5,40 @@ variables = dict()
 
 root = tk.Tk()
 root.title("Shipping Statement")
+root.geometry("420x350+500+300")
+#root.maxsize(500,350)
 
+# Main Frame 
 drf = ttk.Frame(root)
 drf.grid(padx=10,sticky=(tk.E+tk.W))
 
-# Shipping Frame
-shippingFrame = ttk.LabelFrame(drf,text="Shipping Information")
-shippingFrame.grid(row=0,column=0)
-
-ttk.Label(shippingFrame,text="Date").grid(row=0,column=0)
-ttk.Label(shippingFrame,text="From").grid(row=0,column=1)
-ttk.Label(shippingFrame,text="To").grid(row=0,column=2)
+#Top Right Date Frame
+dateFrame = ttk.LabelFrame(drf)
+dateFrame.grid(row=0,column=0)
 
 variables['Date'] = tk.StringVar()
-ttk.Entry(shippingFrame,textvariable=variables['Date']).grid(row=1,column=0)
-variables['From'] = tk.StringVar()
-ttk.Entry(shippingFrame,textvariable=variables['From']).grid(row=1,column=1)
-variables['To'] = tk.StringVar()
-ttk.Entry(shippingFrame,textvariable=variables['To']).grid(row=1,column=2)
+ttk.Label(dateFrame,text="Date:").grid(row=0,column=0, sticky=(tk.E+tk.W))
+ttk.Entry(dateFrame,textvariable=variables['Date']).grid(row=1,column=0)
 
-#Informatinon Frame
-infoFrame = ttk.LabelFrame(drf,text="Info")
-infoFrame.grid(row=1,column=0)
+variables['DriverID'] = tk.StringVar()
+ttk.Label(dateFrame,text="Driver ID").grid(row=0,column=1)
+ttk.Entry(dateFrame,textvariable=variables['DriverID']).grid(row=1,column=1)
+
+# B2B Info Frame
+shippingFrame = ttk.LabelFrame(drf,text="B2B Information")
+shippingFrame.grid(row=1,column=0,pady=10)
+
+ttk.Label(shippingFrame,text="From").grid(row=0,column=0)
+ttk.Label(shippingFrame,text="To").grid(row=0,column=1)
+
+variables['From'] = tk.StringVar()
+ttk.Entry(shippingFrame,textvariable=variables['From']).grid(row=1,column=0)
+variables['To'] = tk.StringVar()
+ttk.Entry(shippingFrame,textvariable=variables['To']).grid(row=1,column=1)
+
+#Shipment Frame
+infoFrame = ttk.LabelFrame(drf,text="Shipment Info")
+infoFrame.grid(row=2,column=0)
 
 ttk.Label(infoFrame,text="Boxes").grid(row=0,column=0)
 ttk.Label(infoFrame,text="Charge").grid(row=0,column=1)
@@ -36,8 +48,11 @@ ttk.Entry(infoFrame,textvariable=variables['Boxes']).grid(row=1,column=0)
 variables['Charge'] = tk.StringVar()
 ttk.Entry(infoFrame,textvariable=variables['Charge']).grid(row=1,column=1)
 
-###
-ttk.Button(text="Clear").grid(row=2,column=0)
-ttk.Button(text="Add").grid(row=2,column=1)
+### Submit Form
+btnFrame = ttk.Frame(root)
+btnFrame.grid(row=3,column=0)
+
+ttk.Button(btnFrame,text="Clear").grid(row=3,column=0)
+ttk.Button(btnFrame,text="Add").grid(row=3,column=1)
 
 root.mainloop()
